692c1743cbd36aa0909cb1f9937918077624f540
Eugene Povidailov <povidailoff@yandex.ru>
2022-07-14 13:22:10 +0300

    sort dict

--- emoji_redactor.py
+++ emoji_redactor.py
@@ -1,16 +1,17 @@
 from emoji_pack import emojies
 
 
-def emoji_add(text):
+def emoji_add(text, emojies_count=1):
     response = []
     for word in text.split():
         response.append(word)
         for spell in emojies:
             if word.startswith(spell):
-                response.append(emojies[spell] * 3)
+                response.append(emojies[spell] * emojies_count)
     return response
 
 
 if __name__ == '__main__':
-    text = 'инопланетянин'
-    print(*emoji_add(text))
+    text = input('Введите текст: ')
+    emojies_count = int(input('Количество повторений эмоджи: '))
+    print(*emoji_add(text, emojies_count))
