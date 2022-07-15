8e588cb7a991994f7005ce948f21378ff2fcdd60
Eugene Povidailov <povidailoff@yandex.ru>
2022-07-15 05:10:35 +0300

    more words

--- emoji_redactor.py
+++ emoji_redactor.py
@@ -1,7 +1,8 @@
 from emoji_pack import emojies
 
 
-def emoji_add(text, emojies_count=1):
+def emoji_add(text: str, emojies_count: int = 1) -> str:
+    """Функция добавляет эмоджи после имеющихся в словаре слов."""
     response = []
     for word in text.split():
         response.append(word)
@@ -14,4 +15,5 @@ def emoji_add(text, emojies_count=1):
 if __name__ == '__main__':
     text = input('Введите текст: ')
     emojies_count = int(input('Количество повторений эмоджи: '))
+    emojies_count = 2
     print(*emoji_add(text, emojies_count))
